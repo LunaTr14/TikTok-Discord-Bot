e1d3592487d0537e7d4179e976df593cff1a08ea
Luna <>
2024-08-24 18:51:50 +1000

    fix: Updated discord bot and client intents

--- bot.py
+++ bot.py
@@ -4,7 +4,8 @@ import discord, requests, os, sys, traceback, json, asyncio, re, random, subproc
 
 sys.path.append("./libs/")
 
-bot = discord.Bot(intents=discord.Intents.all())
+bot_intents = discord.Intents.default()
+bot = discord.Client(intents=bot_intents)
 
 ###
 USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"
